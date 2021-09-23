d1243c879ce47d7526364ed09e7360779ee1bf99
Naohiro Fujie <naohiro.fujie@eidentity.jp>
2021-09-23 16:03:04 +0900

    init

---
+++ app.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBfIvJaqDcY7UfSlUZv76kuwFOdR5GhEtw",
+  authDomain: "sylvan-byway-170107.firebaseapp.com",
+  projectId: "sylvan-byway-170107",
+  storageBucket: "sylvan-byway-170107.appspot.com",
+  messagingSenderId: "833613337725",
+  appId: "1:833613337725:web:403215e36afc6a3b0d0d14"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
